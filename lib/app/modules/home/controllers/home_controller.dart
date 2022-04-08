f5a44ed1d6a149b0e31ace0b5ac94c08d71952b1
Shubham Sinha <shubhamsinha2009@gmail.com>
2022-04-08 12:08:10 +0530

    in app update

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,9 +1,9 @@
 import '../../no_internet/controllers/no_internet_controller.dart';
-
 import '../../../data/model/user_model.dart';
 import '../../../data/provider/firestore_data.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
+import 'package:in_app_update/in_app_update.dart';
 
 class HomeController extends GetxController {
   final isFileDeviceOpen = true.obs;
@@ -24,6 +24,12 @@ class HomeController extends GetxController {
       final String _uid = auth.currentUser?.uid ?? '';
       user(await firestoreData.getUser(_uid));
     }
+
+    if ((await InAppUpdate.checkForUpdate()).updateAvailability ==
+        UpdateAvailability.updateAvailable) {
+      await InAppUpdate.performImmediateUpdate();
+    }
+
     super.onInit();
   }
 }
